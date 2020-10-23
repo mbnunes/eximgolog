b1ac8e95667783bfa9a9fc2f687831471a460a49
Mauricio Nunes <mutila@gmail.com>
2020-10-23 14:54:31 -0300

    add html templates and launch to vscode

--- main.go
+++ main.go
@@ -3,27 +3,49 @@ package main
 import (
 	eximgolog "eximgolog/tools"
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
+var templates *template.Template
+
 func ReadMainlogHandler(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	w.WriteHeader(http.StatusOK)
-	var mylog eximgolog.LogLine
-	eximgolog.InsertLogLine(mylog)
+	//var mylog eximgolog.LogLine
+	//eximgolog.InsertLogLine(mylog)
+	teste := eximgolog.ReadLog("mainlog")
+	for _, t := range teste {
+		eximgolog.InsertLogLine(t)
+	}
 	fmt.Fprintf(w, "Category: %v\n", vars["category"])
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	return
+func indexGetHandler(w http.ResponseWriter, r *http.Request) {
+	templates.ExecuteTemplate(w, "index.html", nil)
+}
+
+func indexPostHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	/*
+		data := r.PostForm.Get("data")
+		horario := r.PostForm.Get("horario")
+		mailid := r.PostForm.Get("mailid")
+		tipo := r.PostForm.Get("tipo")
+	**/
+	http.Redirect(w, r, "/", 302)
 }
 
 func main() {
+
+	templates = template.Must(template.ParseGlob("templates/*.html"))
+
 	r := mux.NewRouter()
-	r.HandleFunc("/", handler)
+	r.HandleFunc("/", indexGetHandler).Methods("GET")
+	r.HandleFunc("/", indexPostHandler).Methods("POST")
 	r.HandleFunc("/readmainlog", ReadMainlogHandler).Methods("GET")
 
 	log.Fatal(http.ListenAndServe("localhost:8080", r))
